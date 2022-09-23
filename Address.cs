cb555e873575caef6ac8265c75e0329e326b6b51
bertoldimatteo <bertoldi.matteo660@gmail.com>
2022-09-23 16:56:31 +0200

    aggiunta soluzione di stampa

---
+++ Address.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+using static System.Net.Mime.MediaTypeNames;
+
+class Address
+{
+    public string name;
+    public string surname;
+    public string street;
+    public string city;
+    public string province; 
+    public int zip;
+
+    public Address(string name, string surname, string street, string city, string province, int zip)
+    {
+        this.name = name;
+        this.surname = surname;
+        this.street = street;
+        this.city = city;
+        this.province = province;
+        this.zip = zip;
+    }
+
+    public string printAddress()
+    {
+        string text = "------ Address ------\n";
+        text += "Name:\t" + this.name + "\n";
+        text += "Surname:\t" + this.surname + "\n";
+        text += "Street:\t" + this.street + "\n";
+        text += "City:\t" + this.city + "\n";
+        text += "Province:\t" + this.province + "\n";
+        text += "Zip:\t" + this.zip + "\n";
+        text += "-------------------";
+
+        return text;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,10 +1,57 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-StreamReader file = File.OpenText("../../../addresses.csv");
+StreamReader addresses = File.OpenText("../../../addresses.txt");
+List<Address> resultList = new List<Address>();
+addresses.ReadLine();
 
-while (!file.EndOfStream)
+while (!addresses.EndOfStream)
 {
-    string address = file.ReadLine();
-    Console.WriteLine(address);
+    //lettura dati come stringa
+    string row = addresses.ReadLine();
+    //divisione dati prendendo come riferimento virgola
+    string[] split = row.Split(',');
+
+    try
+    {
+        //definisco le variabili
+
+        string name = split[0];
+        string surname = split[1];
+        string street = split[2];
+        string city = split[3];
+        string province = split[4];
+        int zip = int.Parse(split[5]);
+
+        Address address = new Address(name, surname, street, city, province, zip);
+        resultList.Add(address);
+
+        Console.WriteLine("Address {0}", name);
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine("Indirizzo non valido " + row);
+    }
+    
 }
-file.Close();
+addresses.Close();
+
+try
+{
+    StreamWriter newAddress = File.CreateText("Formatted Address.txt");
+
+    newAddress.WriteLine("Formatted address with invalid address");
+
+    foreach (Address address in resultList)
+    {
+        Console.WriteLine("Saving {0}", address.name);
+        newAddress.WriteLine();
+        newAddress.WriteLine(address.printAddress());
+    }
+
+    newAddress.Close();
+
+}
+catch (Exception e)
+{
+    Console.WriteLine(e.Message);
+}
